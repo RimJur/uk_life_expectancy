290f19baa0fa0b4d76772afe1ab7c4de7b135375
RimJur <rimgaudas.jurgaitis@gmail.com>
2025-01-04 16:47:47 +0200

    fixing data representation

--- scripts/imputations.py
+++ scripts/imputations.py
@@ -14,14 +14,14 @@ imputed_df = imputed_df.select(
     pl.col("year").cast(pl.Int64),
     pl.col("life_expectancy"),
     pl.col("gdp").cast(pl.Int64),
-    pl.col("relative_health_expenditure"),
-    pl.col("urbanization_rate"),
-    pl.col("daily_caloric_intake"),
-    pl.col("average_working_hours"),
-    pl.col("child_mortality"),
+    pl.col("relative_health_expenditure").round(2),
+    pl.col("urbanization_rate").round(2),
+    pl.col("daily_caloric_intake").round(2),
+    pl.col("average_working_hours").round(2),
+    pl.col("child_mortality").round(2),
     pl.col("ww1").cast(pl.Int64),
     pl.col("ww2").cast(pl.Int64),
-    pl.col("lighting_price"),
+    pl.col("lighting_price").round(2),
 )
 imputed_df
 imputed_df.write_csv("../data/final.csv")
